0601a94dea8021e9cdd720d560b9461d9f9011a6
piti3 <patrik.ravnak@gmail.com>
2018-10-16 09:41:46 +0200

    Initial commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+
+    public function about(){
+        return view('about');
+    }
+
+    public function helloworld(){
+        return view('helloworld');
+    }
+}
---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>Podatki o uporabnikih</h1>
+<br />
+<table border="1">
+    <tr>
+        <td>Id</td>
+        <td>Ime</td>
+        <td>E-mail</td>
+        <td>Vloga</td>
+        <td>Ustvarjen</td>
+        <td>Stanje</td>
+    </tr>
+
+@if($users)
+
+@foreach($users as $user)
+    <tr>
+        <td>{{$user->id}}</td>
+        <td>{{$user->name}}</td>
+        <td>{{$user->email}}</td>
+        <td>{{$user->role->name}}</td>
+        <td>{{$user->created_at->diffForHumans()}}</td>
+        <td>@if(isset($user->is_active))
+                @if($user->is_active)
+                    Aktiven
+                @else
+                    Neaktiven
+                @endif
+            @endif
+        </td>
+    </tr>
+
+
+@endforeach
+
+@endif
+
+</table>
+
+@endsection
